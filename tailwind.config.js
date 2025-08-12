ceb9b0ff5a25d5b067155c6e74e0e7f61a454005
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-08-12 11:59:09 -0500

    refactor: added tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./js/**/*.js",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
